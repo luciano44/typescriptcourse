c7db93c126e3a8686cbfffc82e7d273f439b4eba
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-09-01 03:54:36 -0300

    finishes lesson11 to-do list app following the course

--- lesson11/src/main.ts
+++ lesson11/src/main.ts
@@ -1,6 +1,55 @@
 import "./css/style.css"
 import FullList from "./model/FullList"
+import ListItem from "./model/ListItem"
+import ListTemplate from "./templates/ListTemplate"
 
+const initApp = (): void => {
+  const fullList = FullList.instance
+  const template = ListTemplate.instance
+
+  const itemEntryForm = document.querySelector(
+    "#itemEntryForm"
+  ) as HTMLFormElement
+
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent) => {
+    e.preventDefault()
+    const input = document.querySelector("#newItem") as HTMLInputElement
+    const newEntryText: string = input.value.trim()
+    if (!newEntryText) return
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1
+
+    const newItem = new ListItem(itemId.toString(), newEntryText)
+
+    fullList.addItem(newItem)
+
+    template.render(fullList)
+  })
+
+  const clearItems = document.querySelector(
+    "#clearItemsButton"
+  ) as HTMLButtonElement
+
+  clearItems.addEventListener("click", () => {
+    fullList.clearList()
+    template.clear()
+  })
+
+  fullList.load()
+  template.render(fullList)
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
+
+//
+//
+//////////////////////////////////////////////////
+//       MY SOLUTION
+//////////////////////////////////////////////////
+
+/*
 const fullList = new FullList()
 
 //PREVENT DEFAULT REFRESHING FORM
@@ -17,6 +66,4 @@ form.addEventListener("submit", (e) => {
 clearItemsBtn.addEventListener("click", () => {
   fullList.clearList()
 })
-
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
+*/
--- lesson11/src/model/FullList.ts
+++ lesson11/src/model/FullList.ts
@@ -10,6 +10,66 @@ interface List {
 }
 
 export default class FullList implements List {
+  static instance: FullList = new FullList()
+  private constructor(private _list: ListItem[] = []) {}
+
+  get list(): ListItem[] {
+    return this._list
+  }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myList")
+    if (typeof storedList !== "string") return
+
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+      JSON.parse(storedList)
+
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._item,
+        itemObj._id,
+        itemObj._checked
+      )
+      FullList.instance.addItem(newListItem)
+    })
+  }
+
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list))
+  }
+
+  clearList(): void {
+    this._list = []
+    this.save()
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj)
+    this.save()
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id)
+    this.save()
+  }
+}
+
+//
+//
+//////////////////////////////////////////////////
+//       MY SOLUTION
+//////////////////////////////////////////////////
+
+/*
+interface List {
+  list: ListItem[]
+  load(): void
+  save(): void
+  clearList(): void
+  addItem(itemObj: ListItem): void
+  removeItem(id: string): void
+}
+export default class FullList implements List {
   private _list: ListItem[] = []
   private static id: number = 0
 
@@ -86,4 +146,4 @@ export default class FullList implements List {
     this._list = this._list.filter((item) => item.id !== id)
     this.load()
   }
-}
+}*/
