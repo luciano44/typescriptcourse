dedbf212426e4320333a705549aef1962ecd0b61
Luciano Ribeiro <luciano.ar6@gmail.com>
2023-09-03 22:55:12 -0300

    add Counter and List components

--- lesson12/src/App.tsx
+++ lesson12/src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import Heading from "./components/Heading"
 import Section from "./components/Section"
+import Counter from "./components/Counter"
+import List from "./components/List"
 
 const App = () => {
+  const [count, setCount] = useState<number>(0)
+
   return (
     <>
       <Heading title="hey buddy" />
-      <Section>Hello</Section>
-      <Section title="hey test">Hello</Section>
+      <Section title="my title">Hello</Section>
+      <Counter setCount={setCount}>Counter is {count}</Counter>
+      <List
+        items={["🍕 Pizza", "🌙 Moon", "🏈 Football"]}
+        render={(item: string) => <span className="bold gold">{item}</span>}
+      />
     </>
   )
 }
---
+++ lesson12/src/components/Counter.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react"
+
+type SimpleCounterProps = {
+  setCount: React.Dispatch<React.SetStateAction<number>>
+  children: ReactNode
+}
+
+const Counter = ({ setCount, children }: SimpleCounterProps) => {
+  return (
+    <>
+      <h1>{children}</h1>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount((prev) => prev - 1)
+        }}
+      >
+        -
+      </button>
+    </>
+  )
+}
+
+export default Counter
